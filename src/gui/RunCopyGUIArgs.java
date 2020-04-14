052cf4d852b5d8d1f0c0364c6d9bdba87f900de1
Adrian Martinez Rodriguez <adrianmrit@yahoo.com>
2020-04-14 19:04:19 -0400

    Unnecesary comments deleted

--- src/gui/RunCopyGUIArgs.java
+++ src/gui/RunCopyGUIArgs.java
@@ -6,13 +6,10 @@ import java.nio.file.Paths;
 import copy.Copy;
 
 public class RunCopyGUIArgs {
-	public static void main(String args[]) {
-		// TODO: avoid this loop
-//		src = new File("/home/adrianmrit/Videos/");
-//		dest = new File("/home/adrianmrit/Videos/test_dest/Videos");
-		
+	public static void main(String args[]) {		
 		Path src = Paths.get("Sample/").toAbsolutePath();
 		Path dest = Paths.get("TestDest/").toAbsolutePath();
+		
 		CopyGUI.setDebug(true);
 		Copy.setDebug(true);
 		CopyGUI.main(new String[] {src.toString(), dest.toString()});
