5ca99f3dd531d54da14ff2f0ce2e62f5a94f9450
Adrian Martinez Rodriguez <adrianmrit@yahoo.com>
2020-04-17 18:37:36 -0400

    Use a hashmap to store the original files and the destination

--- src/copy/Copy.java
+++ src/copy/Copy.java
@@ -1,30 +1,21 @@
 package copy;
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.EventQueue;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.text.BadLocationException;
 
-import gui.FileExistsDialog;
 import gui.LongProgressBarModel;
 
 public class Copy extends Thread{
@@ -33,16 +24,12 @@ public class Copy extends Thread{
 	private final File orig;
 	private final File dest;
 	
-	private final Object[] origPaths;  // TODO: change to Path[]
+//	private final Object[] origPaths;  // TODO: change to Path[]
 	private final long totalSize;
 	
 	private static final int DEFAULT_BUFFER_SIZE = 8192; // 8kb
-	private static final long MAX_BUFFER_SIZE = 1048576; // 1mb
 	
-	private int bufferSize; // current choosed buffer size
-	private short lastAvgTime; // average time between x number of writes to a file, used to calculate bufferSize
-	private static final short NUMBER_STEPS = 10;
-	private short operationCounter;  // counter, should reset when equals NUMBER_STEPS
+	private HashMap<Path, Path> origDestMap;
 	
 	// GUI elements
 	
@@ -56,17 +43,17 @@ public class Copy extends Thread{
 	public Copy(String orig, String dest) throws IOException{
 		this.orig = new File(orig);
 		this.dest = new File(dest);
-		this.bufferSize = DEFAULT_BUFFER_SIZE;
 		
 		if (this.orig.isFile()) {
-			// if file there is only one path to be copied
-			this.origPaths = new Path[] {this.orig.toPath()};
+			
+			this.origDestMap = new HashMap<>();
+			this.origDestMap.put(this.orig.toPath(), resolveDest(this.orig.toPath()));
 		} else {
-			// get all sub-paths to be copied
-			this.origPaths = Copy.getPaths(this.orig.toPath());
+			Iterator<Path> iter = Copy.getPathsIterator(this.orig.toPath());
+			setOrigDestMap(iter);
 		}
 		
-		this.totalSize = Copy.getFolderArraySize(this.origPaths);
+		this.totalSize = Copy.getFolderArraySize(this.origDestMap.keySet());
 	}
 	
 	public Copy(File orig, File dest) throws IOException {
@@ -98,6 +85,14 @@ public class Copy extends Thread{
 		this.hasGUI = b;
 	}
 	
+	public void setOrigDestMap(Iterator<Path> originPaths) {
+		this.origDestMap = new HashMap<>();
+		while (originPaths.hasNext()) {
+			Path originPath = originPaths.next();
+			Path destPath = resolveDest(originPath);
+			this.origDestMap.put(originPath, destPath);
+		}
+	}
 	
 	/** Sets the debug status
 	 * @param debug status
@@ -118,26 +113,20 @@ public class Copy extends Thread{
 		return resolved;
 	}
 	
-	/** Get an Object array that contains all the paths between this folder,
-	 * including paths between sub-folders.
-	 * @param path
-	 * @return Object[] with all sub-paths
-	 * @throws IOException if fails
-	 */
-	public static Object[] getPaths(Path path) throws IOException {
-		Object [] paths = Files.walk(path).toArray();
-		return paths;
+	public static Iterator<Path> getPathsIterator(Path path) throws IOException {
+		return Files.walk(path).iterator();
 	}
 	
-	/** Get the total size for all the paths between an Object array,
+	/** Get the total size for all the paths between a Set,
 	 * if it is a file.
-	 * @param paths Object array with all the paths
+	 * @param paths all the paths
 	 * @return long total size
 	 */
-	public static long getFolderArraySize(Object[] paths) {
+	public static long getFolderArraySize(Set<Path> paths) {
 		long total = 0;
-		for (int i=0;i<paths.length;i++) {
-			File path = ((Path) paths[i]).toFile();
+		Iterator<Path> i = paths.iterator();
+		while(i.hasNext()) {
+			File path = i.next().toFile();
 			if (path.isFile()) {
 				total += path.length();
 			}
@@ -151,7 +140,6 @@ public class Copy extends Thread{
 	 */
 	public void run() {
 		int exitStatus = 0;
-		launchWindowsTest();
 		try {
 			long took = System.currentTimeMillis();
 //			System.out.println(launchWindowsTest());
@@ -173,26 +161,6 @@ public class Copy extends Thread{
 		}
 	}
 	
-	private static GridBagConstraints getConstraints(int gridx, int gridy, int gridwidth, int gridheight, int anchor,
-			int fill) {
-			Insets insets = new Insets(3,3,3,3);
-			GridBagConstraints gbc = new GridBagConstraints(gridx, gridy,
-			gridwidth, gridheight, 1.0, 1.0, anchor, fill, insets, 0, 0);
-			return gbc;
-	}
-	
-	public void launchWindowsTest() {
-		FileExistsDialog dialog = new FileExistsDialog(this.frame, "example");
-		dialog.run();
-		System.out.println(dialog.getAction());
-		try {
-			System.out.println(dialog.getInputValue());
-		} catch (BadLocationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	/** Copy the files and creates the folders
 	 * @throws IOException if fails
 	 */
@@ -202,9 +170,11 @@ public class Copy extends Thread{
 		// TODO: handle copy in same path, should duplicate file with a "(copy)" at the end,
 		// TODO: Avoid copy folder into itself
 		// before file extension.
-		for (int i=0; i<this.origPaths.length; i++) {
-			Path origPath = (Path) this.origPaths[i];
-			Path destPath = this.resolveDest(origPath);
+		Iterator<Path> originIterator = this.origDestMap.keySet().iterator();
+		while (originIterator.hasNext()) {
+			Path origPath = originIterator.next();
+			Path destPath = this.origDestMap.get(origPath);
+			
 			if (origPath.toFile().isFile()) {
 				// TODO: handle file exists
 				this.copyFile(origPath, destPath);
@@ -246,7 +216,7 @@ public class Copy extends Thread{
 		
 		try {
 			// do the copy
-			byte[] buf = new byte[this.bufferSize]; // TODO: Find optimal chunk size
+			byte[] buf = new byte[DEFAULT_BUFFER_SIZE]; // TODO: Find optimal chunk size
 			int bytesRead;
 			
 			while ((bytesRead = is.read(buf)) > 0) {
