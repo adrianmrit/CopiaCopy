c52592cb6ce576d3388f8bc641f033bca150e490
Adrian Martinez Rodriguez <adrianmrit@yahoo.com>
2020-04-18 11:22:19 -0400

    Some refactoring

--- src/copy/Copy.java
+++ src/copy/Copy.java
@@ -28,7 +28,6 @@ public class Copy extends Thread{
 	private final File origin;
 	private final File dest;
 	
-//	private final Object[] origPaths;  // TODO: change to Path[]
 	private long totalSize = 0;
 	
 	private static final int DEFAULT_BUFFER_SIZE = 8192; // 8kb
@@ -47,7 +46,6 @@ public class Copy extends Thread{
 	private JLabel currentLabel;
 	private JFrame frame; // used to create windows
 	private boolean hasGUI = false;
-	
 	
 	public Copy(String orig, String dest) throws IOException{
 		this.origin = new File(orig);
@@ -71,27 +69,97 @@ public class Copy extends Thread{
 		this(orig.toString(), dest.toString());
 	}
 	
+	/** 
+	 * Sets the frame. Useful for creating dialogs.
+	 * @param frame
+	 */
 	public void setFrame(JFrame frame) {
 		this.frame = frame;
 	}
 	
+	/** 
+	 * Sets the file progress model
+	 * @param fileProgressModel
+	 */
 	public void setFileProgressModel(LongProgressBarModel fileProgressModel) {
 		this.fileProgressModel = fileProgressModel;
 	}
 	
+	/**
+	 * Sets the totalProgressModel
+	 * @param totalProgressModel
+	 */
 	public void setTotalProgressModel(LongProgressBarModel totalProgressModel) {
 		this.totalProgressModel = totalProgressModel;
 	}
 	
+	/** 
+	 * Sets the currentLabel
+	 * @param currentLabel
+	 */
 	public void setCurrentLabel(JLabel currentLabel) {
 		this.currentLabel = currentLabel;
 	}
 	
+	/** 
+	 * Set hasGUI. If there is GUI it will be updated
+	 * @param b
+	 */
 	public void setHasGUI(boolean b) {
 		this.hasGUI = b;
 	}
 	
-	public void setOrigDestMap(Iterator<Path> originPaths) {
+	/** Sets the debug status
+	 * @param debug status
+	 */
+	public static void setDebug(boolean debug) {
+		DEBUG = debug;
+	}
+	
+	/** Resolves the <i>path</i> where path will be copied.
+	 * @param path origin path
+	 * @return destination path
+	 */
+	public Path resolveDest(Path path) {
+		String pathRootParent = this.origin.getParent();
+		String subPath = path.toString().replaceFirst(pathRootParent, "");  // path - this.orig parent
+		Path resolved = Paths.get(this.dest.toString(), subPath);  // join both paths
+		
+		return resolved;
+	}
+	
+	/** Returns an Iterator for all the inner files and folder between a path.
+	 * @param path Path to be walked
+	 * @return Iterator<Path> with all the inner paths
+	 * @throws IOException
+	 */
+	private static Iterator<Path> getPathsIterator(Path path) throws IOException {
+		return Files.walk(path).iterator();
+	}
+	
+	/** Get the total size for all the paths between a Set,
+	 * if it is a file.
+	 * @param paths all the paths
+	 * @return long total size
+	 */
+	private static long getFolderArraySize(Set<Path> paths) {
+		long total = 0;
+		Iterator<Path> i = paths.iterator();
+		while(i.hasNext()) {
+			File path = i.next().toFile();
+			if (path.isFile()) {
+				total += path.length();
+			}
+		}
+		
+		return total;
+	}
+	
+	/**
+	 * Populate the map with all the origins and resolved destinations
+	 * @param originPaths
+	 */
+	private void setOrigDestMap(Iterator<Path> originPaths) {
 		this.origDestMap = new TreeMap<>();
 		while (originPaths.hasNext()) {
 			Path originPath = originPaths.next();
@@ -128,7 +196,6 @@ public class Copy extends Thread{
 				System.out.println("Took (get size): " + tookGetSize + "ms");
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			exitStatus = 1;
 		}
@@ -138,7 +205,8 @@ public class Copy extends Thread{
 		}
 	}
 	
-	/** Copy the files and creates the folders
+	/** 
+	 * Copy the files and creates the folders
 	 * @throws IOException if fails
 	 */
 	public void copy() throws IOException {
@@ -152,12 +220,90 @@ public class Copy extends Thread{
 			Path destPath = this.origDestMap.get(origPath);
 			
 			if (origPath.toFile().isFile()) {
-				// TODO: handle file exists
-				this.copyFile(origPath, destPath);
+				this.copyFile(origPath.toFile(), destPath.toFile());
 			} else {
 				destPath.toFile().mkdirs();
 			}
 		}
+	}
+	
+	/** 
+	 * Copy a file from origin to destination, and updates the UIElements.
+	 * @param orig origin path
+	 * @param dest destination path
+	 * @throws IOException if fails
+	 */
+	public void copyFile(File origF, File destF) throws IOException {
+		destF.getParentFile().mkdirs();
+		
+		InputStream is = new FileInputStream(origF);;
+		OutputStream os = new FileOutputStream(destF);
+		resetUICopyFile(origF, destF);
+		
+		try {
+			copyFileHelper(is, os); // does the copy
+		} finally {
+			is.close();
+			os.close();
+		}
+	}
+	
+	/** 
+	 * Does the actual copy
+	 * @param is InputStream
+	 * @param os OutputStream
+	 */
+	private void copyFileHelper(InputStream is, OutputStream os) throws IOException {
+		byte[] buf = new byte[DEFAULT_BUFFER_SIZE]; // TODO: Find optimal chunk size
+		int bytesRead;
+		
+		while ((bytesRead = is.read(buf)) > 0) {
+			os.write(buf, 0, bytesRead);
+			updateProgressBars(bytesRead);
+		}
+	}
+	
+	/** 
+	 * Resets the fileProgressBar, and the currentLabel
+	 * @param originF
+	 * @param destF // not used for now
+	 */
+	private void resetUICopyFile(File origF, File destF) {
+		if (this.hasGUI) {
+			this.fileProgressModel.setLongMaximum(origF.length()); // resets the fileProgressBar
+			this.fileProgressModel.setLongValue(0); // resets the fileProgressBar
+			this.currentLabel.setText("Current: " + origF.getName());
+		}
+	}
+	
+	/** 
+	 * Updates the progress bars if they exist
+	 * @param bytesRead last number of bytes read
+	 */
+	private void updateProgressBars(long bytesRead) {
+		if (this.hasGUI) {
+			this.fileProgressModel.addLongValue(bytesRead);
+			this.totalProgressModel.addLongValue(bytesRead);
+		}
+	}
+	
+	/**
+	 * Check if there is any file in the copy that exists in the origin. If it does it will ask what to do with it.
+	 * Files will be renamed and removed after asking for all of them, to avoid concurrent operation errors.
+	 */
+	private void resolveExistingFiles() {  // TODO: resolve existing files while populating origDestMap
+		Iterator<Path> originIterator = this.origDestMap.keySet().iterator();
+		this.toRename = new TreeMap<>();
+		this.toRemove = new ArrayList<>();
+		while (originIterator.hasNext()) {
+			File origin = originIterator.next().toFile();
+			File dest = this.origDestMap.get(origin.toPath()).toFile();
+			if (dest.exists() && dest.isFile()) {
+				handleFileExistDialog(origin, dest);
+			}
+		}
+		renameFilesInMap();
+		removeFilesFromMap();
 	}
 	
 	/**
@@ -172,7 +318,7 @@ public class Copy extends Thread{
 			String action = dialog.getAction();
 			switch (action) {
 				case FileExistsDialog.CANCEL:
-					System.exit(0);  // exit copy
+					System.exit(0);  // exit copy// TODO: handle file exists
 				
 				case FileExistsDialog.SKIP:
 					this.toRemove.add(origin.toPath());
@@ -196,127 +342,19 @@ public class Copy extends Thread{
 			Path origin = toRenameIterator.next();
 			this.origDestMap.replace(origin, this.toRename.get(origin));
 		}
+		this.toRename = null; // clean unnecessary space in memory
 	}
 	
 	/**
 	 * Removes the files that where selected to be removed with FileExistsDialog
 	 */
 	private void removeFilesFromMap() {
-		Iterator<Path> toRemoveIterator = toRemove.iterator();
+		Iterator<Path> toRemoveIterator = this.toRemove.iterator();
 		while (toRemoveIterator.hasNext()) {
 			Path origin = toRemoveIterator.next();
 			this.origDestMap.remove(origin);
 		}
-	}
-	
-	/**
-	 * Check if there is any file in the copy that exists in the origin. If it does it will ask what to do with it.
-	 * Files will be renamed and removed after asking for all of them, to avoid concurrent operation errors.
-	 */
-	private void resolveExistingFiles() {
-		Iterator<Path> originIterator = this.origDestMap.keySet().iterator();
-		this.toRename = new TreeMap<>();
-		this.toRemove = new ArrayList<>();
-		while (originIterator.hasNext()) {
-			File origin = originIterator.next().toFile();
-			File dest = this.origDestMap.get(origin.toPath()).toFile();
-			if (dest.exists() && dest.isFile()) {
-				handleFileExistDialog(origin, dest);
-			}
-		}
-		renameFilesInMap();
-		removeFilesFromMap();
-	}
-	
-	/** Sets the debug status
-	 * @param debug status
-	 */
-	public static void setDebug(boolean debug) {
-		DEBUG = debug;
-	}
-	
-	/** Resolves the <i>path</i> where path will be copied.
-	 * @param path origin path
-	 * @return destination path
-	 */
-	public Path resolveDest(Path path) {
-		String pathRootParent = this.origin.getParent();
-		String subPath = path.toString().replaceFirst(pathRootParent, "");  // path - this.orig parent
-		Path resolved = Paths.get(this.dest.toString(), subPath);  // join both paths
 		
-		return resolved;
-	}
-	
-	/** Returns an Iterator for all the inner files and folder between a path.
-	 * @param path Path to be walked
-	 * @return Iterator<Path> with all the inner paths
-	 * @throws IOException
-	 */
-	public static Iterator<Path> getPathsIterator(Path path) throws IOException {
-		return Files.walk(path).iterator();
-	}
-	
-	/** Get the total size for all the paths between a Set,
-	 * if it is a file.
-	 * @param paths all the paths
-	 * @return long total size
-	 */
-	public static long getFolderArraySize(Set<Path> paths) {
-		long total = 0;
-		Iterator<Path> i = paths.iterator();
-		while(i.hasNext()) {
-			File path = i.next().toFile();
-			if (path.isFile()) {
-				total += path.length();
-			}
-		}
-		
-		return total;
-	}
-
-	/** Updates the progress bars if they exist
-	 * @param bytesRead last number of bytes read
-	 */
-	private void updateProgressBars(long bytesRead) {
-		if (this.hasGUI) {
-			this.fileProgressModel.addLongValue(bytesRead);
-			this.totalProgressModel.addLongValue(bytesRead);
-		}
-	}
-	
-	/** Copy a file from orig to dest, and updates the UIElements.
-	 * @param orig origin path
-	 * @param dest destination path
-	 * @throws IOException if fails
-	 */
-	public void copyFile(Path orig, Path dest) throws IOException {
-		File origF = orig.toFile();
-		File destF = dest.toFile();
-		destF.getParentFile().mkdirs();
-		
-		InputStream is = new FileInputStream(origF);;
-		OutputStream os = new FileOutputStream(destF);
-		
-		if (this.hasGUI) {
-			this.fileProgressModel.setLongMaximum(origF.length()); // resets the fileProgressBar
-			this.fileProgressModel.setLongValue(0); // resets the fileProgressBar
-	
-			this.currentLabel.setText("Current: " + orig.toString());
-		}
-		
-		try {
-			// do the copy
-			byte[] buf = new byte[DEFAULT_BUFFER_SIZE]; // TODO: Find optimal chunk size
-			int bytesRead;
-			
-			while ((bytesRead = is.read(buf)) > 0) {
-				os.write(buf, 0, bytesRead);
-				updateProgressBars(bytesRead);
-			}
-
-		} finally {
-			is.close();
-			os.close();
-		}
+		this.toRemove = null; // clean unnecessary space in memory
 	}
 }
