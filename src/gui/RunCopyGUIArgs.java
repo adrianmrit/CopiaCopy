bfff35ddf79134e1cef9d0fad137876298d72a93
Adrian Martinez Rodriguez <adrianmrit@yahoo.com>
2020-04-29 13:40:10 -0400

    Important comment

--- src/gui/RunCopyGUIArgs.java
+++ src/gui/RunCopyGUIArgs.java
@@ -10,10 +10,10 @@ import testFiles.FileFactory;
 public class RunCopyGUIArgs {
 	public static void main(String args[]) {		
 		Path src = FileFactory.TEST_FOLDER;
-		Path dest = FileFactory.TEST_DEST_FOLDER_PARENT;
+		Path dest = FileFactory.TEST_DEST_FOLDER_PARENT; // must exist
 
 		CopyGUI.setDebug(true);
-		CopyGUI copy = new CopyGUI(src.toString(), dest.toString(), Copiable.CUT_MODE);
+		CopyGUI copy = new CopyGUI(src.toString(), dest.toString(), Copiable.COPY_MODE);
 		Copy.setDebug(true);
 		copy.run();
 	}
